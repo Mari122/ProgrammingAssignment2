4ff463ee7cc08b1ca09957b9a35cd1770f56a04e
Mari122 <66797648+Mari122@users.noreply.github.com>
2020-06-12 11:11:39 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,41 @@
-makeVector <- function(x = numeric()) {
-       m <- NULL
-       set <- function(y) {
-              x <<- y
-              m <<- NULL
-       }
-       get <- function() x
-       setmean <- function(mean) m <<- mean
-       getmean <- function() m
-       list(set = set, get = get,
-            setmean = setmean,
-            getmean = getmean)
+  
+# This is the Lexical Scoping assignment as per requirement of the course "R Programming" on Coursera
+
+# The function "makeCacheMatrix" creates a special "matrix" object that has the ability to cache its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(inv) {
+    i <<- inv
+  }
+  
+  getInverse <- function() i
+  
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-cachemean <- function(x, ...) {
-       m <- x$getmean()
-       if(!is.null(m)) {
-              message("getting cached data")
-              return(m)
-       }
-       data <- x$get()
-       m <- mean(data, ...)
-       x$setmean(m)
-       m
+#function - "cacheSolve" computes the inverse of special "matrix" created by "makeCacheMatrix"
+
+cacheSolve <- function(x, ...) {
+  # Return a matrix that is the inverse of 'x'
+  
+  i <- x$getInverse()
+  
+  if(!is.null(i)) {
+    message("getting cached inverse")
+    return(i)
+  }
+  
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setInverse(i)
+  i
 }
